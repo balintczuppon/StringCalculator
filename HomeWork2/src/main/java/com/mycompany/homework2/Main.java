42f3a5f857de4b4ebc8483a39127af2e89a21d28
Czuppon Bálint Péter <balintczuppon@gmail.com>
2015-02-21 12:33:41 +0100

    A metódus üres stringet és tetszőleges számú számot fogad tetszőleges választó karakterrel és a negatív számokat kezeli.

--- HomeWork2/src/main/java/com/mycompany/homework2/Main.java
+++ HomeWork2/src/main/java/com/mycompany/homework2/Main.java
@@ -5,21 +5,28 @@
  */
 package com.mycompany.homework2;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Admin
  */
 public class Main {
     
-    public static void main(String[] agrs){
-        
-        StringCalculator calculator = new StringCalculator();
-        
-        System.out.println(calculator.add(""));
-        System.out.println(calculator.add("2"));
-        System.out.println(calculator.add("2,3"));
-        System.out.println(calculator.add("2,3,10,4,30"));
-        System.out.println(calculator.add("1;2"));
-        System.out.println(calculator.add("2,3;10\n4|30"));
+    public static void main(String[] agrs){  
+        try {
+            StringCalculator calculator = new StringCalculator();
+            
+            System.out.println(calculator.add(""));
+            System.out.println(calculator.add("2"));
+            System.out.println(calculator.add("2,3"));
+            System.out.println(calculator.add("2,3,10,4,30"));
+            System.out.println(calculator.add("1;2"));
+            System.out.println(calculator.add("2,3;10\n4|30"));
+            System.out.println(calculator.add("2,-3;10\n4|-30"));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- HomeWork2/src/main/java/com/mycompany/homework2/StringCalculator.java
+++ HomeWork2/src/main/java/com/mycompany/homework2/StringCalculator.java
@@ -11,22 +11,37 @@ package com.mycompany.homework2;
  */
 public class StringCalculator {
 
-    int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        int sum = 0;
-        
-        numbers = numbers.replaceAll("[^0-9]", ",");
-        
-        String[] array = numbers.split(",");
-        if (array.length == 2) {
-            sum = Integer.valueOf(array[0]) + Integer.valueOf(array[1]);
+    int add(String numbers) throws Exception {
+        try {
+            if (numbers.isEmpty()) {
+                return 0;
+            }
+            int sum = 0;
+            String forbidden = "";
+
+            numbers = numbers.replaceAll("[^0-9-]", ",");
+
+            String[] array = numbers.split(",");
+
+            for (String string : array) {
+                if (Integer.valueOf(string) < 0) {
+                    forbidden += string + ", ";
+                }
+            }
+            if (!forbidden.isEmpty()) {
+                throw new Exception("Negatives not allowed: " + forbidden);
+            }
+
+            if (array.length == 2) {
+                sum = Integer.valueOf(array[0]) + Integer.valueOf(array[1]);
+                return sum;
+            }
+            for (String string : array) {
+                sum += Integer.valueOf(string);
+            }
             return sum;
+        } catch (Exception ex) {
+            throw ex;
         }
-        for (String string : array) {
-            sum += Integer.valueOf(string);
-        }
-        return sum;
     }
 }
